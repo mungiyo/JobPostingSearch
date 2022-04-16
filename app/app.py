b7d21b0f84e02015afedaec00f7e0c005b71031d
장성태 <wkdtjdxo1234@naver.com>
2022-04-16 23:53:39 +0900

    app Update

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from elasticsearch import Elasticsearch
 
 app = Flask(__name__)
@@ -15,10 +15,41 @@ def index():
     }
     
     res = es.search(index=index, body=body)
-    
     sources = [source['_source'] for source in res['hits']['hits']]
     
-    return render_template('index.html', sources=sources)
+    return render_template('index/index.html', sources=sources)
+
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('auth/login.html')
+
+@app.route('/register', methods=['GET'])
+def register():
+    return render_template('auth/register.html')
+
+@app.route('/password', methods=['GET'])
+def password():
+    return render_template('auth/password.html')
+
+@app.route('/tables', methods=['GET'])
+def tables():
+    es = Elasticsearch('localhost:9200')
+    index = 'postings'
+    body = {
+        "size": 1000,
+        "query": {
+            "match_all": {}
+        }
+    }
+    
+    res = es.search(index=index, body=body)
+    sources = [source['_source'] for source in res['hits']['hits']]
+    
+    return render_template('addons/tables.html', sources=sources)
+
+@app.route('/charts', methods=['GET'])
+def charts():
+    return render_template('addons/charts.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
